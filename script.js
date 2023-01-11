63715856ca7bf4691541d861d4ac94f543ac58cb
Guilherme Setim <64696390+guilhermesetim@users.noreply.github.com>
2023-01-11 16:28:01 -0300

    otimização das imagens

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function test() {
+function menuMobile() {
     let viewAtual = document.querySelector('#mobile-menu').style.display
-    
+
     if(viewAtual == 'block'){
         document.querySelector('#mobile-menu').style.display = 'none'
     }
